0dc2a18138f1986a8b6c5b8433a59ee97ce329f1
Alexey Malov <alexey.malov@cpslabs.net>
2016-03-10 21:17:28 +0300

    Реализована возможность регистрации наблюдателей и их уведомления

--- samples/observer/WeatherStation/WeatherData.h
+++ samples/observer/WeatherStation/WeatherData.h
@@ -1,17 +1,53 @@
 ﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class IObserver
+{
+public:
+	virtual void Update() = 0;
+protected:
+	~IObserver() = default;
+};
 
 class CDisplay
 {
 public:
 	void Update(double temp, double humidity, double pressure)
 	{
-
+		std::cout << "Current Temp " << temp << std::endl;
+		std::cout << "Current Hum " << humidity << std::endl;
+		std::cout << "Current Pressure " << pressure << std::endl;
+		std::cout << "----------------" << std::endl;
 	}
 };
 
-class CWeatherData
+class IObservable
 {
 public:
+	virtual ~IObservable() = default;
+	virtual void RegisterObserver(IObserver & observer) = 0;
+	virtual void NotifyObservers() = 0;
+};
+
+class CWeatherData : public IObservable
+{
+public:
+	void RegisterObserver(IObserver & observer) override
+	{
+		m_observers.push_back(observer);
+	}
+
+	void NotifyObservers() override
+	{
+		for (auto & observer : m_observers)
+		{
+			observer.get().Update();
+		}
+	}
 	// Температура в градусах Цельсия
 	double GetTemperature()const
 	{
@@ -39,7 +75,17 @@ public:
 		m_forecastDisplay.Update(temp, humidity, pressure);
 	}
 
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+		m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+		MeasurementsChanged();
+	}
+
 private:
+	vector<reference_wrapper<IObserver>> m_observers;
+
 	CDisplay m_currentConditionsDisplay;
 	CDisplay m_statisticsDisplay;
 	CDisplay m_forecastDisplay;
--- samples/observer/WeatherStation/main.cpp
+++ samples/observer/WeatherStation/main.cpp
@@ -1,6 +1,9 @@
+#include "WeatherData.h"
 
-
-void main()
+int main()
 {
-
+	CWeatherData wd;
+	wd.SetMeasurements(3, 0.7, 760);
+	wd.SetMeasurements(4, 0.8, 761);
+	return 0;
 }
