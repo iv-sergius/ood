23f7df12c3100013ccf5b16c45294e0e8dfb8820
Alexey Malov <alexey.malov@cpslabs.net>
2016-03-17 22:23:28 +0300

    Добавили кучу разных добавок к напиткам

--- samples/decorator/coffee/main.cpp
+++ samples/decorator/coffee/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <memory>
+#include <functional>
 
 using namespace std;
 
@@ -113,38 +114,159 @@ protected:
 class CLemon : public CCondimentDecorator
 {
 public:
-	CLemon(unique_ptr<IBeverage> && beverage)
+	CLemon(unique_ptr<IBeverage> && beverage, unsigned quantity = 1)
 		: CCondimentDecorator(move(beverage))
+		, m_quantity(quantity)
 	{}
 protected:
 	double GetCondimentCost()const override
 	{
-		return 10;
+		return 10 * m_quantity;
 	}
 	string GetCondimentDescription()const override
 	{
-		return "Lemon";
+		return "Lemon x " + to_string(m_quantity);
 	}
+private:
+	unsigned m_quantity;
+};
+
+enum class IceCubeType
+{
+	Dry,
+	Water
+};
+
+class CIceCubes : public CCondimentDecorator
+{
+public:	
+	CIceCubes(unique_ptr<IBeverage> && beverage, unsigned quantity, IceCubeType type = IceCubeType::Water)
+		: CCondimentDecorator(move(beverage))
+		, m_quantity(quantity)
+		, m_type(type)
+	{}
+protected:
+	double GetCondimentCost()const override
+	{
+		return (m_type == IceCubeType::Dry ? 10 : 5) * m_quantity;
+	}
+	string GetCondimentDescription()const override
+	{
+		return string(m_type == IceCubeType::Dry ? "Dry" : "Water") + " Ice Cube x " + to_string(m_quantity);
+	}
+private:
+	unsigned m_quantity;
+	IceCubeType m_type;
+};
+
+enum class SyrupType
+{
+	Chocolate,
+	Maple
+};
+
+class CSyrup : public CCondimentDecorator
+{
+public:
+	CSyrup(unique_ptr<IBeverage> && beverage, SyrupType syrupType)
+		: CCondimentDecorator(move(beverage))
+		, m_syrupType(syrupType)
+	{}
+protected:
+	double GetCondimentCost()const override
+	{
+		return 15;
+	}
+	string GetCondimentDescription()const override
+	{
+		return string(m_syrupType == SyrupType::Chocolate ? "Chocolate" : "Maple") + " syrup";
+	}
+private:
+	SyrupType m_syrupType;
+};
+
+class CChocolateCrumbs : public  CCondimentDecorator
+{
+public:
+	CChocolateCrumbs(unique_ptr<IBeverage> && beverage, unsigned quantity)
+		:CCondimentDecorator(move(beverage))
+		, m_quantity(quantity)
+	{
+
+	}
+	double GetCondimentCost()const override
+	{
+		return 2 * m_quantity;
+	}
+	string GetCondimentDescription()const override
+	{
+		return "Chocolate crumbs " + to_string(m_quantity) + "g";
+	}
+private:
+		unsigned m_quantity;
+};
+
+
+class CCoconutFlakes : public CCondimentDecorator
+{
+public:
+	CCoconutFlakes(unique_ptr<IBeverage> && beverage, unsigned mass)
+		: CCondimentDecorator(move(beverage))
+		, m_mass(mass)
+	{}
+
+protected:
+	double GetCondimentCost()const override
+	{
+		return 1 * m_mass;
+	}
+	string GetCondimentDescription()const override
+	{
+		return "Coconut flakes " + to_string(m_mass) + "g";
+	}
+private:
+	unsigned m_mass;
 };
 
 /*
-struct MakeLemon 
+Функциональный объект, создающий лимонную добавку
+*/
+struct MakeLemon
 {
-	auto operator()(unique_ptr<IBeverage> && beverage)const { return make_unique<CLemon>(move(beverage)); }
-};
-struct MakeCinnamon
-{
-	auto operator()(unique_ptr<IBeverage> && beverage)const { return make_unique<CCinnamon>(move(beverage)); }
-};*/
+	MakeLemon(unsigned quantity):m_quantity(quantity){}
 
-template <typename Condiment>
-struct MakeCondiment
-{
-	auto operator()(unique_ptr<IBeverage> && b)const
+	auto operator()(unique_ptr<IBeverage> && beverage)const
 	{
-		return make_unique<Condiment>(move(b));
+		return make_unique<CLemon>(move(beverage), m_quantity); 
 	}
+private:
+	unsigned m_quantity;
 };
+
+/*
+Функция, возвращающая функцию, создающую коричную добавку
+*/
+function<unique_ptr<IBeverage>(unique_ptr<IBeverage> &&)> MakeCinnamon()
+{
+	return [] (unique_ptr<IBeverage> && b) {
+		return make_unique<CCinnamon>(move(b));
+	};
+}
+
+/*
+Функция, которая возвращает функцию, конструирующую произвольную добавку
+Аргументы функции MakeCondiment захватываются возвращаемой функцией и *передаются* 
+конструктору указанной добавки следом за декорируемым напитком
+
+Используются variadic templates
+*/
+template <typename Condiment, typename... Ts>
+function<unique_ptr<IBeverage>(unique_ptr<IBeverage>&&)> MakeCondiment(Ts&&...args)
+{
+	return [=](unique_ptr<IBeverage> && b) mutable {
+		return make_unique<Condiment>(move(b), forward<Ts>(args)...);
+	};
+}
 
 /*
 unique_ptr<IBeverage> operator << (unique_ptr<IBeverage> && lhs, const MakeLemon & factory)
@@ -168,7 +290,13 @@ int main()
 {
 	auto beverage = make_unique<CCoffee>() 
 		<< MakeCondiment<CCinnamon>() 
-		<< MakeCondiment<CLemon>();
+		<< MakeCondiment<CLemon>(2)
+		<< MakeCondiment<CIceCubes>(2, IceCubeType::Dry)
+		<< MakeCondiment<CChocolateCrumbs>(2);
+
+	auto milkshakeWithMapleSyrup = make_unique<CMilkshake>()
+		<< MakeCondiment<CSyrup>(SyrupType::Maple)
+		<< MakeCondiment<CCoconutFlakes>(8);
 
 	/*
 	auto coffee = make_unique<CCoffee>();
@@ -177,4 +305,5 @@ int main()
 	*/
 
 	cout << beverage->GetDescription() << " costs " << beverage->GetCost() << endl;
+	cout << milkshakeWithMapleSyrup->GetDescription() << " costs " << milkshakeWithMapleSyrup->GetCost() << endl;
 }
