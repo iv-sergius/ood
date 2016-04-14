e9ff27e41497f67f9ce1037bfd3e8aa8d3f95c8a
Alexey Malov <alexey.malov@ispringsolutions.com>
2016-04-14 18:52:04 +0300

    Добавлен пример шаблонной реализации синглтона

--- samples/singleton/singleton/main.cpp
+++ samples/singleton/singleton/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
+#include <memory>
+#include <mutex>
 
 using namespace std;
 
@@ -9,8 +11,8 @@ class CMeyersSingleton
 public:
 	static CMeyersSingleton & GetInstance()
 	{
-		static CMeyersSingleton instance;
-		return instance;
+		static CMeyersSingleton Instance;
+		return Instance;
 	}
 
 	void DoSomething()
@@ -75,16 +77,61 @@ ILogger & GetDefaultLogger()
 	return logger;
 }
 
+template<class T>
+class CSingleton
+{
+public:
+	static T& Instance()
+	{
+		call_once(m_flag, [] {
+			m_instance.reset(new T());
+		});
+		return *m_instance;
+	}
+protected:
+	~CSingleton() {}
+	CSingleton() {}
+private:
+	CSingleton(const CSingleton&) = delete;
+	CSingleton& operator=(const CSingleton&) = delete;
+
+	static unique_ptr<T> m_instance;
+	static once_flag m_flag;
+};
+template<class T> unique_ptr<T> CSingleton<T>::m_instance;
+template<class T> once_flag CSingleton<T>::m_flag;
+
+class CMyClass : public CSingleton<CMyClass>
+{
+	friend class CSingleton<CMyClass>;
+public:
+	void DoSomething()
+	{
+		cout << "CMyClass::DoSomething" << endl;
+	}
+private:
+	CMyClass()
+	{
+	}
+};
+
 int main()
 {
-	auto & instance = CMeyersSingleton::GetInstance();
-	instance.DoSomething();
+	{
+		auto & instance = CMeyersSingleton::GetInstance();
+		instance.DoSomething();
+	}
 
 	{
 		auto & logger = GetDefaultLogger();
 		logger.LogMessage("this message will not be logged");
 		logger.Enable(true);
 		logger.LogMessage("This message will be logged");
+	}
+
+	{
+		auto & inst = CMyClass::Instance();
+		inst.DoSomething();
 	}
 	return 0;
 }
