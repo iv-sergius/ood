08bf13367dc63943ff2459d7c90d8c61e8be2399
Alexey Malov <alexey.malov@cpslabs.net>
2016-05-17 01:53:25 +0300

    Добавлена заготовка для лабораторной работы по паттерну "Адаптер"

---
+++ samples/adapter/lwtask/main.cpp
@@ -0,0 +1,210 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <boost/format.hpp>
+
+using namespace std;
+
+// Пространство имен графической библиотеки (недоступно для изменения)
+namespace graphics_lib
+{
+// Холст для рисования
+class ICanvas
+{
+public:
+	virtual void MoveTo(int x, int y) = 0;
+	virtual void LineTo(int x, int y) = 0;
+	virtual ~ICanvas() = default;
+};
+
+// Реализация холста для рисования
+class CCanvas : public ICanvas
+{
+	virtual void MoveTo(int x, int y) override
+	{
+		cout << "MoveTo (" << x << ", " << y << ")" << endl;
+	}
+	virtual void LineTo(int x, int y) override
+	{
+		cout << "LineTo (" << x << ", " << y << ")" << endl;
+	}
+};
+}
+
+// Пространство имен библиотеки для рисования фигур (использует graphics_lib)
+// Код библиотеки недоступен для изменения
+namespace shape_drawing_lib
+{
+struct Point
+{
+	int x;
+	int y;
+};
+
+// Интерфейс объектов, которые могут быть нарисованы на холсте из graphics_lib
+class ICanvasDrawable
+{
+public:
+	virtual void Draw(graphics_lib::ICanvas & canvas)const = 0;
+	virtual ~ICanvasDrawable() = default;
+};
+
+class CTriangle : public ICanvasDrawable
+{
+public:
+	CTriangle(const Point & p1, const Point & p2, const Point & p3)
+	{
+		// TODO: написать код конструктора
+	}
+
+	void Draw(graphics_lib::ICanvas & canvas)const override
+	{
+		// TODO: написать код рисования треугольника на холсте
+	}
+private:
+	// TODO: дописать приватную часть
+};
+
+class CRectangle : public ICanvasDrawable
+{
+public:
+	CRectangle(const Point & leftTop, int width, int height)
+	{
+		// TODO: написать код конструктора
+	}
+
+	void Draw(graphics_lib::ICanvas & canvas)const override
+	{
+		// TODO: написать код рисования прямоугольника на холсте
+	}
+private:
+	// TODO: дописать приватную часть 
+};
+
+// Художник, способный рисовать ICanvasDrawable-объекты на ICanvas
+class CCanvasPainter
+{
+public:
+	CCanvasPainter(graphics_lib::ICanvas & canvas)
+	{
+		// TODO: дописать конструктор класса
+	}
+	void Draw(const ICanvasDrawable & drawable)
+	{
+		// TODO: дописать код рисования ICanvasDrawable на canvas, переданном в конструктор
+	}
+private:
+	// TODO: дописать приватную часть
+};
+}
+
+// Пространство имен современной графической библиотеки (недоступно для изменения)
+namespace modern_graphics_lib
+{
+class CPoint
+{
+	CPoint(int x, int y) :x(x), y(y) {}
+
+	int x;
+	int y;
+};
+
+// Класс для современного рисования графики
+class CModernGraphicsRenderer
+{
+public:
+	CModernGraphicsRenderer(ostream & strm) : m_out(strm)
+	{
+	}
+
+	~CModernGraphicsRenderer()
+	{
+		if (m_drawing) // Завершаем рисование, если оно было начато
+		{
+			EndDraw();
+		}
+	}
+
+	// Этот метод должен быть вызван в начале рисования
+	void BeginDraw()
+	{
+		if (m_drawing)
+		{
+			throw logic_error("Drawing has already begun");
+		}
+		m_out << "<draw>" << endl;
+		m_drawing = true;
+	}
+
+	// Выполняет рисование линии
+	void DrawLine(const CPoint & start, const CPoint & end)
+	{
+		if (!m_drawing)
+		{
+			throw logic_error("DrawLine is allowed between BeginDraw()/EndDraw() only");
+		}
+		m_out << boost::format(R"(  <line fromX="%1%" fromY="%2" toX="%3%" toY="%4%"/>)") << endl;
+	}
+
+	// Этот метод должен быть вызван в конце рисования
+	void EndDraw()
+	{
+		if (!m_drawing)
+		{
+			throw logic_error("Drawing has not been started");
+		}
+		m_out << "</draw>" << endl;
+		m_drawing = false;
+	}
+private:
+	ostream & m_out;
+	bool m_drawing = false;
+};
+}
+
+// Пространство имен приложения (доступно для модификации)
+namespace app
+{
+void PaintPicture(shape_drawing_lib::CCanvasPainter & painter)
+{
+	using namespace shape_drawing_lib;
+
+	CTriangle triangle({ 10, 15 }, { 100, 200 }, { 150, 250 });
+	CRectangle rectangle({ 30, 40 }, 18, 24);
+
+	// TODO: нарисовать прямоугольник и треугольник при помощи painter
+}
+
+void PaintPictureOnCanvas()
+{
+	graphics_lib::CCanvas simpleCanvas;
+	shape_drawing_lib::CCanvasPainter painter(simpleCanvas);
+	PaintPicture(painter);
+}
+
+void PaintPictureOnModernGraphicsRenderer()
+{
+	modern_graphics_lib::CModernGraphicsRenderer renderer(cout);
+	(void)&renderer; // устраняем предупреждение о неиспользуемой переменной
+
+	// TODO: при помощи существующей функции PaintPicture() нарисовать
+	// картину на renderer
+	// Подсказка: используйте паттерн "Адаптер"
+}
+}
+
+int main()
+{
+	cout << "Should we use new API (y)?";
+	string userInput;
+	if (getline(cin, userInput) && (userInput == "y" || userInput == "Y"))
+	{
+		app::PaintPictureOnModernGraphicsRenderer();
+	}
+	else
+	{
+		app::PaintPictureOnCanvas();
+	}
+
+	return 0;
+}
