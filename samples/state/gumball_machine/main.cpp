21795091d70fa6d55dc18dfc3f196b952214dd0a
Alexey Malov <alexey.malov@cpslabs.net>
2016-06-01 00:17:37 +0300

    Реализованы 2 версии машины для выдачи жвачки - с enum и с паттерном State

---
+++ samples/state/gumball_machine/main.cpp
@@ -0,0 +1,61 @@
+#include "NaiveGumBallMachine.h"
+#include "GumBallMachineWithState.h"
+
+using namespace std;
+
+template <typename MachineType>
+void TestGumballMachine(MachineType & m)
+{
+	cout << m.ToString() << endl;
+
+	m.InsertQuarter();
+	m.TurnCrank();
+
+	cout << m.ToString() << endl;
+
+	m.InsertQuarter();
+	m.EjectQuarter();
+	m.TurnCrank();
+
+	cout << m.ToString() << endl;
+
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.EjectQuarter();
+
+	cout << m.ToString() << endl;
+
+	m.InsertQuarter();
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.InsertQuarter();
+	m.TurnCrank();
+	m.InsertQuarter();
+	m.TurnCrank();
+
+	cout << m.ToString() << endl;
+}
+
+void TestNaiveGumballMachine()
+{
+	naive::CGumballMachine m(5);
+	TestGumballMachine(m);
+}
+
+void TestGumballMachineWithState()
+{
+	with_state::CGumballMachine m(5);
+	TestGumballMachine(m);
+}
+
+int main()
+{
+	TestNaiveGumballMachine();
+
+	cout << "\n-----------------\n";
+	TestGumballMachineWithState();
+
+	return 0;
+}
