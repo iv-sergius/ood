399acebcae66e3c9f6e4068c3d77bda66d96bac2
Sergey Ivanov <iv.sergius@yandex.ru>
2018-09-20 21:33:15 +0300

    On #1
    Signed-off-by: Sergey Ivanov <iv.sergius@yandex.ru>

--- labs/02/WeatherStation/WeatherData.h
+++ labs/02/WeatherStation/WeatherData.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <string>
 #include "Observer.h"
 
 using namespace std;
@@ -15,7 +16,7 @@ struct SWeatherInfo
 	double pressure = 0;
 };
 
-template <typename TData >
+template <typename TData>
 class CParameterStats
 {
 public:
@@ -26,10 +27,17 @@ public:
 		m_accValue += value;
 		++m_countValue;
 	}
-	void PrintStatistic(std::ostream &outStream) {
-		outStream << "Max: " << std::setw(4) << m_maxValue 
-			<< "    Min: " << std::setw(4) << m_minValue  
-			<< "    Average: " << std::setw(5) << m_accValue / m_countValue << std::endl;
+	TData GetMin()const
+	{
+		return m_minValue;
+	}
+	TData GetMax()const
+	{
+		return m_maxValue;
+	}
+	TData GetAverage()const
+	{
+		return m_accValue / m_countValue;
 	}
 private:
 	TData m_minValue = std::numeric_limits<TData>::max();
@@ -61,6 +69,14 @@ private:
 	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
 	остается публичным
 	*/
+	template <typename TData>
+	void PrintParameter(CParameterStats<TData> const& parameter, const std::string const& parameterName)
+	{
+		std::cout << std::setw(12) << parameterName << " : "
+			<< " Max: " << std::setw(5) << parameter.GetMax()
+			<< " Min: " << std::setw(5) << parameter.GetMin()
+			<< " Average: " << std::setw(6) << parameter.GetAverage() << std::endl;
+	}
 	void Update(SWeatherInfo const& data) override
 	{
 		m_humidity.AddValue(data.humidity);
@@ -68,12 +84,9 @@ private:
 		m_temperature.AddValue(data.temperature);
 
 		std::cout << "---- Update StatsDisplay ----" << std::endl;
-		std::cout << "Humidity     ";
-		m_humidity.PrintStatistic(std::cout);
-		std::cout << "Pressure     "; 
-		m_pressure.PrintStatistic(std::cout);
-		std::cout << "Temperature  ";
-		m_temperature.PrintStatistic(std::cout);
+		PrintParameter(m_humidity, "Humidity");
+		PrintParameter(m_pressure, "Pressure");
+		PrintParameter(m_temperature, "Temperature");
 	}
 	CParameterStats<double> m_humidity;
 	CParameterStats<double> m_pressure;
