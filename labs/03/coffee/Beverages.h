f924e6e1df43bc21cc3df5df1dd985bfe0915268
Sergey Ivanov <iv.sergius@yandex.ru>
2018-09-27 17:25:33 +0300

    Add new beverages
    Signed-off-by: Sergey Ivanov <iv.sergius@yandex.ru>

--- labs/03/coffee/Beverages.h
+++ labs/03/coffee/Beverages.h
@@ -6,84 +6,154 @@
 class CBeverage : public IBeverage
 {
 public:
-	CBeverage(const std::string & description)
-		:m_description(description)
+	CBeverage(const std::string & description, const double cost)
+		:m_description(description),
+		m_cost(cost)
 	{}
 
 	std::string GetDescription()const override final
 	{
 		return m_description;
 	}
+	double GetCost()const final
+	{
+		return m_cost;
+	}
 private:
 	std::string m_description;
+	double m_cost;
 };
 
 // Кофе
 class CCoffee : public CBeverage
 {
 public:
-	CCoffee(const std::string& description = "Coffee")
-		:CBeverage(description) 
+	CCoffee(const std::string& description = "Coffee", const double cost = 80)
+		:CBeverage(description, cost) 
 	{}
-
-	double GetCost() const override 
-	{
-		return 60; 
-	}
 };
 
 // Капуччино
 class CCapuccino : public CCoffee
 {
 public:
-	CCapuccino() 
-		:CCoffee("Capuccino") 
+	CCapuccino(const std::string& description = "Capuccino", const double cost = 80)
+		:CCoffee(description, cost) 
 	{}
+};
 
-	double GetCost() const override 
-	{
-		return 80; 
-	}
+class CStandartCapuccino : public CCapuccino
+{
+public:
+	CStandartCapuccino()
+		:CCapuccino("Standart Capuccino")
+	{}
+};
+
+
+class CBigCapuccino : public CCapuccino
+{
+public:
+	CBigCapuccino()
+		:CCapuccino("Big Capuccino", 120)
+	{}
 };
 
 // Латте
 class CLatte : public CCoffee
 {
 public:
-	CLatte() 
-		:CCoffee("Latte") 
+	CLatte(const std::string& description = "Latte", const double cost = 90)
+		:CCoffee(description, cost) 
 	{}
+};
 
-	double GetCost() const override 
-	{
-		return 90; 
-	}
+class CStandartLatte : public CLatte
+{
+public:
+	CStandartLatte()
+		:CLatte("Standart Latte", 130)
+	{}
+};
+
+class CBigLatte : public CLatte
+{
+public:
+	CBigLatte()
+		:CLatte("Big Latte", 130)
+	{}
 };
 
 // Чай
 class CTea : public CBeverage
 {
 public:
-	CTea() 
-		:CBeverage("Tea") 
+	CTea(const std::string& description = "Tea")
+		:CBeverage("Tea", 30) 
 	{}
+};
 
-	double GetCost() const override 
-	{
-		return 30; 
-	}
+class CGreenTea : public CTea
+{
+public:
+	CGreenTea()
+		:CTea("Green Tea")
+	{}
+};
+
+class CEarlGreyTea : public CTea
+{
+public:
+	CEarlGreyTea()
+		:CTea("Earl Grey Tea")
+	{}
+};
+
+class COolongTea : public CTea
+{
+public:
+	COolongTea()
+		:CTea("Oolong Tea")
+	{}
+};
+
+class CPuerTea : public CTea
+{
+public:
+	CPuerTea()
+		:CTea("Puer Tea")
+	{}
 };
 
 // Молочный коктейль
 class CMilkshake : public CBeverage
 {
 public:
-	CMilkshake() 
-		:CBeverage("Milkshake") 
+	CMilkshake(const std::string& description = "Milkshake", const double cost = 80)
+		:CBeverage("Milkshake", 80) 
 	{}
+};
 
-	double GetCost() const override 
-	{ 
-		return 80; 
-	}
+class CSmallMilkshake : public CMilkshake
+{
+public:
+	CSmallMilkshake()
+		:CMilkshake("Small Milkshake", 50)
+	{}
+};
+
+class CStandartMilkshake : public CMilkshake
+{
+public:
+	CStandartMilkshake()
+		:CMilkshake("Standart Milkshake", 60)
+	{}
+};
+
+class CBigMilkshake : public CMilkshake
+{
+public:
+	CBigMilkshake()
+		:CMilkshake("Big Milkshake", 80)
+	{}
 };
